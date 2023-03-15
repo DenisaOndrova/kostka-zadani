979f8cbc4e71e40d470f5a973f5a88f45ba87061
DenisaO <ondrovadenisa95@gmail.com>
2023-03-15 20:23:53 +0100

    Homework2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('funguju!');
+//ukol 2
+
+let number = 1;
+const dice = document.querySelector('.dice');
+const side = () =>{
+    number = number+1
+    if (number ===7){
+        number=1
+    }
+    dice.src=`img/side${number}.svg`
+}
+document.addEventListener('keydown', side)
